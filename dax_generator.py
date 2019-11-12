1b3d891db51ebe765df703bba7708b0bffc62106
Rafael Ferreira da Silva <rafsilva@isi.edu>
2019-11-11 16:07:47 -0800

    transforming it into hierarchical workflows

--- dax_generator.py
+++ dax_generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import hashlib
 import HTMLParser
 import json
 import os
@@ -24,25 +25,55 @@ with open(options.config) as config_file:
   # Executables
   e_simulator = Executable('wrench_simulator', arch='x86_64', installed=True)
   e_simulator.addPFN(PFN('file:///simulator/task_clustering_batch_simulator/simulator', 'local'))
+  e_simulator.addProfile(Profile(Namespace.PEGASUS, key="clusters.size", value=100))
   workflow.addExecutable(e_simulator)
 
-  # simulation jobs
-  for tf in data['trace_files']:
+  subwf_dir = "subwfs"
+  os.mkdir(subwf_dir)
 
-    output_file = "output-file.json"
-    j = Job("wrench_simulator")
-    j.addProfile(Profile(Namespace.CONDOR, key="+SingularityImage", value=html_parser.unescape("&quot;/cvmfs/singularity.opensciencegrid.org/wrenchproject/task-clustering:latest&quot;")))
-    j.addArguments(tf[1]) # num_compute_nodes
-    j.addArguments("/simulator/trace_files/" + tf[0]) # job trace file
-    j.addArguments("") # max jobs in system
-    j.addArguments("") # workflow specification
-    j.addArguments("") # workflow start time
-    j.addArguments("") # algorithm
-    j.addArguments("") # batch algorithm
-    j.addArguments("--wrench-no-log")
-    j.addArguments(output_file) # json file to write results to
-    j.uses(File(output_file), link=Link.OUTPUT, transfer=True)
-    workflow.addJob(j)
+  # simulation jobs
+  for wf in data['workflows']:
+
+    # generate sub workflow per workflow
+    subwf_id = "subwf_" + wf.replace(".dax", "")
+    subwf = ADAG(subwf_id)
+
+    for tf in data['trace_files']:
+      for max_sys_jobs in data['max_sys_jobs']:
+        for st in data['start_times']:
+          for alg in data['algorithms']:
+            output_file = wf + "_" + hashlib.md5(tf[1] + tf[0] + max_sys_jobs + st + alg).hexdigest() + ".json"
+            j = Job("wrench_simulator")
+            j.addProfile(Profile(Namespace.CONDOR, key="+SingularityImage", value=html_parser.unescape("&quot;/cvmfs/singularity.opensciencegrid.org/wrenchproject/task-clustering:latest&quot;")))
+            j.addArguments(tf[1]) # num_compute_nodes
+            j.addArguments(data['trace_file_dir'] + "/" + tf[0]) # job trace file
+            j.addArguments(max_sys_jobs) # max jobs in system
+            j.addArguments(data['workflow_type'] + ":" + data['workflow_dir'] + "/" + wf) # workflow specification
+            j.addArguments(st) # workflow start time
+            j.addArguments(alg) # algorithm
+            j.addArguments("conservative_bf") # batch algorithm
+            j.addArguments("--wrench-no-log")
+            j.addArguments(output_file) # json file to write results to
+            j.uses(File(output_file), link=Link.OUTPUT, transfer=True)
+            subwf.addJob(j)
+    
+    # write subworkflow DAX file
+    with open(subwf_dir + "/" + subwf_id + ".xml", "w") as subwf_out:
+      subwf.writeXML(subwf_out)
+    
+    subwf_dax = File(subwf_id + ".xml")
+    subwf_dax.addPFN(PFN("file://" + os.getcwd() + "/" + subwf_dir + "/" + subwf_id + ".xml", "local"))
+    workflow.addFile(subwf_dax)
+
+    subwf_job = DAX(subwf_id + ".xml", id=subwf_id)
+    subwf_job.addProfile(Profile("dagman", "CATEGORY", "subwf"))
+    subwf_job.uses(subwf_dax)
+    subwf_job.addArguments("-Dpegasus.catalog.site.file=" + os.getcwd() + "/sites.xml",
+                           "--sites", "condor_pool",
+                           "--output-site", "local",
+                           "--cluster", "horizontal",
+                           "--cleanup", "inplace")
+    workflow.addDAX(subwf_job)
 
   # Write the DAX to file
   f = open(options.daxfile, "w")
